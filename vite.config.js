dbf390697bd2fd469142657e637994e3e78a0229
shadyibrahim9876-oss <shadyibrahim9876@gmail.com>
2025-12-14 01:41:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",  // <--- ضيف السطر ده مهم جداً عشان المسارات
+  base: "./", 
 })
